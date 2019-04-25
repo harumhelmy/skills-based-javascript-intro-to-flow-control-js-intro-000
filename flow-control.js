d4272ab9336ad68553d312edf6084a35cbd29797
Harum Helmy <harumendhah@gmail.com>
2019-04-25 20:03:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,5 +27,27 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  case 13:
+    console.log("You are a teenager");
+    break;
+  case 14:
+    console.log("You are a teenager");
+    break;
+  case 15:
+    console.log("You are a teenager");
+    break;
+  case 16:
+    console.log("You are a teenager");
+    break;
+  case 17:
+    console.log("You are a teenager");
+    break;
+  case 18:
+    console.log("You are a teenager");
+    break;
+  case 18:
+    console.log("You are a teenager");
+    break;
+  default:
+    console.log("You have an age");
 }
